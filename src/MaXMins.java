7443d098b7da46d9c9eef1887a0b29e0769703d9
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-31 12:57:09 +0900

    수정

--- src/MaXMins.java
+++ src/MaXMins.java
@@ -5,15 +5,15 @@ public class MaXMins {
         Scanner myObj = new Scanner(System.in); // Create a Scanner object
 
         try {
-            System.out.print("Enter the first number: ");
+            System.out.print("Enter the first number: "); // Enter the first number
             int num1 = myObj.nextInt();
 
-            System.out.print("Enter the second number: ");
+            System.out.print("Enter the second number: "); // Enter the second number
             int num2 = myObj.nextInt();
 
-            int max = Math.max(num1, num2);
+            int max = Math.max(num1, num2); // Choose Maximum number between two numbers
             System.out.println("The maximum number is: " + max);
-            
+
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
         }
