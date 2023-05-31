ada4b9c8ded90c927115aadca704e68f48d2383b
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-31 13:19:33 +0900

    수정

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class MaxMins {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); // Create a Scanner object
-
         try {
-            System.out.print("Enter the first number: "); // Enter the first number
-            int num1 = myObj.nextInt();
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = myObj.nextInt(); // 첫 번째 숫자 입력
 
-            System.out.print("Enter the second number: "); // Enter the second number
-            int num2 = myObj.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = myObj.nextInt(); // 두 번째 숫자 입력
 
-            int max = Math.max(num1, num2); // Choose Maximum number between two numbers
-            System.out.println("The maximum number is: " + max);
+            int max = getMax(num1, num2); // getMax 메서드를 호출하여 max 값을 구함
 
+            System.out.println("두 숫자 중 최댓값은 " + max + "입니다.");
         } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
+            System.out.println("올바른 숫자를 입력해주세요.");
         }
+    }
+
+    // 두 숫자 중 최댓값을 구하는 메서드
+    public static int getMax(int num1, int num2) {
+        return Math.max(num1, num2);
     }
 }
